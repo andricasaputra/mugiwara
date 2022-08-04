a77c0473d25dd2360f8fc815df3743e89e66343f
andricasaputra <dric.dev@gmail.com>
2022-08-04 22:41:18 +0800

    room

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -32,7 +32,7 @@ class Order extends Model
 
     public function room()
     {
-        return $this->belongsTo(Room::class);
+        return $this->belongsTo(Room::class, 'room_id');
     }
 
     public function user()
