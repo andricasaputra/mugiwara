956f1c109ecec4849adae8144387ede5b48dc975
andricasaputra <dric.dev@gmail.com>
2022-08-28 15:13:48 +0800

    changer type table

--- app/Http/Controllers/Api/XenditCallbackController.php
+++ app/Http/Controllers/Api/XenditCallbackController.php
@@ -10,17 +10,59 @@ class XenditCallbackController extends Controller
 {
     public function ewallet(Request $request)
     {
-        $callback = CallbackXendit::create([
-            'payload' => $request->all()
-        ]);
+        $url = 'https://api.xendit.co/callback_urls';
+          $apiKey = env('XENDIT_SECRET_KEY');
+          $headers = [];
+          $headers[] = 'Content-Type: application/json';
+          $data = [
+              'url' => 'https://www.xendit.co/callback_catcher'
+          ];
 
-        return $callback;
+          $curl = curl_init();
+
+          $payload = json_encode($data);
+          curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+          curl_setopt($curl, CURLOPT_USERPWD, $apiKey.":");
+          curl_setopt($curl, CURLOPT_URL, $url);
+          curl_setopt($curl, CURLOPT_POST, true);
+          curl_setopt($curl, CURLOPT_POSTFIELDS, $payload);
+          curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+          $result = curl_exec($curl);
+          //echo $result;
+
+            $callback = CallbackXendit::create([
+                'payload' => $result
+            ]);
+
+            return $callback;
     }
 
     public function ovo(Request $request)
     {
+        $url = 'https://api.xendit.co/callback_urls';
+          $apiKey = env('XENDIT_SECRET_KEY');
+          $headers = [];
+          $headers[] = 'Content-Type: application/json';
+          $data = [
+              'url' => 'https://www.xendit.co/callback_catcher'
+          ];
+
+          $curl = curl_init();
+
+          $payload = json_encode($data);
+          curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+          curl_setopt($curl, CURLOPT_USERPWD, $apiKey.":");
+          curl_setopt($curl, CURLOPT_URL, $url);
+          curl_setopt($curl, CURLOPT_POST, true);
+          curl_setopt($curl, CURLOPT_POSTFIELDS, $payload);
+          curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+          $result = curl_exec($curl);
+          //echo $result;
+
         $callback = CallbackXendit::create([
-            'payload' => $request->all()
+            'payload' => $results
         ]);
 
         return $callback;
