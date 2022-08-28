91551967d22af630668a9ad7f9e6ee270366280f
andricasaputra <dric.dev@gmail.com>
2022-08-28 15:54:41 +0800

    changer type table

--- app/Http/Controllers/Api/XenditCallbackController.php
+++ app/Http/Controllers/Api/XenditCallbackController.php
@@ -4,35 +4,61 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\CallbackXendit;
+use App\Models\Payments\Ewallet;
 use Illuminate\Http\Request;
 
 class XenditCallbackController extends Controller
 {
     public function ewallet(Request $request)
     {
-        $data = $request->all();
+        $data = json_encode($request->all());
         \Log::info($data);
 
         $callback = CallbackXendit::create([
             'payload' => $data
         ]);
 
+        $status = json_decode($callback->payload);
+
+        $ewallet = Ewallet::where('ewallet_id', $status?->data?->id)->first();
+
+        $ewallet->update([
+            'payload' => $data
+        ]);
+
+        $ewallet->payment()->update([
+            'status' => $status?->data?->status,
+            'amount' => $status?->data?->charge_amount,
+        ]);
     }
 
     public function ovo(Request $request)
     {
-        $data = $request->all();
+        $data = json_encode($request->all());
         \Log::info($data);
 
         $callback = CallbackXendit::create([
             'payload' => $data
+        ]);
+        
+        $status = json_decode($callback->payload);
+
+        $ewallet = Ewallet::where('ewallet_id', $status?->data?->id)->first();
+
+        $ewallet->update([
+            'payload' => $data
+        ]);
+
+        $ewallet->payment()->update([
+            'status' => $status?->data?->status,
+            'amount' => $status?->data?->charge_amount,
         ]);
     }
 
     public function virtualAccount(Request $request)
     {
         $callback = CallbackXendit::create([
-            'payload' => $request->all()
+            'payload' => json_encode($request->all())
         ]);
 
         return $callback;
