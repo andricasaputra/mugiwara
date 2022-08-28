75cb31e8ca5d8ad18905e98db1401355859c3a5a
andricasaputra <dric.dev@gmail.com>
2022-08-28 14:54:35 +0800

    changer type table

--- database/migrations/2022_08_27_214718_create_callback_xendits_table.php
+++ database/migrations/2022_08_27_214718_create_callback_xendits_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('callback_xendits', function (Blueprint $table) {
             $table->id();
-            $table->json('payload');
+            $table->longText('payload');
             $table->timestamps();
         });
     }
