406434a2fd9c9e028c069bf6fd98735ab883326e
andricasaputra <dric.dev@gmail.com>
2022-09-17 04:04:39 +0800

    fix bug

--- app/Http/Controllers/Admin/ProductUserController.php
+++ app/Http/Controllers/Admin/ProductUserController.php
@@ -59,8 +59,8 @@ class ProductUserController extends Controller
                     'image' => $filename
                 ]);
 
-                $redeem->transaction_number = $request->no_resi;
-                $redeem->no_resi = $request->no_resi;
+                $redeem->transaction_number = $redeem->transaction_number ?? Random::generate(10, 1234567890);
+                $redeem->no_resi = $redeem->transaction_number;
                 $redeem->status = $request->status;
                 $redeem->jenis_pengiriman = $request->jenis_pengiriman;
                 $redeem->save();
