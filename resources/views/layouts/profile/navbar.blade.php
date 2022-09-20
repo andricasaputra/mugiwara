cfcd5ad8644bcf0bd37d606dd806d8f42177b8c1
bayudava29 <bayudava16@gmail.com>
2022-09-20 20:07:18 +0700

    update:button login

--- resources/views/layouts/profile/navbar.blade.php
+++ resources/views/layouts/profile/navbar.blade.php
@@ -21,7 +21,7 @@
 								<a class="nav-link {{url()->full() == $m->url_menu ? 'active' : ''}}" href="{{ $m->url_menu }}">{{$m->nama_menu}}</a>
 							@endforeach
 						@endif
-						<a class="nav-link only" href="https://capsuleinn.id/admin/login">Masuk</a>
+						<a class="nav-link only" href="{{route('login')}}">Masuk</a>
 					</div>
 				</div>
 			</div>
@@ -44,11 +44,11 @@
 			</div>
 			<div class="offcanvas-body">
 				<div class="navbar-nav">
-					<a class="nav-link active" href="{{ route('profile.home') }}}">Beranda</a>
-					<a class="nav-link" href="{{ route('profile.mitra') }}">Jadi Mitra</a>
-					<a class="nav-link" href="{{ route('profile.hotel') }}">Hotel</a>
-					<a class="nav-link" href="{{ route('profile.tentang') }}">Tentang Kami</a>
-					<a class="nav-link" href="{{ route('profile.bantuan') }}">Bantuan</a>
+					@if(count($menu) != 0)
+						@foreach($menu as $k => $m)
+							<a class="nav-link {{url()->full() == $m->url_menu ? 'active' : ''}}" href="{{ $m->url_menu }}">{{$m->nama_menu}}</a>
+						@endforeach
+					@endif
 					<a class="nav-link only" href="{{ route('login') }}">Masuk</a>
 				</div>
 			</div>
