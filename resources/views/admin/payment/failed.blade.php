0631e5eb6cc1bcd3c310f9066dcb0f847d13f4b4
andricasaputra <dric.dev@gmail.com>
2022-09-22 14:58:14 +0800

    fixing bugs

--- resources/views/admin/payment/failed.blade.php
+++ resources/views/admin/payment/failed.blade.php
@@ -1,6 +1,7 @@
 <html>
   <head>
     <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,400i,700,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   </head>
     <style>
       body {
@@ -9,28 +10,26 @@
         background: #EBF0F5;
       }
         h1 {
-          color: #88B04B;
+          color: #ff0000;
           font-family: "Nunito Sans", "Helvetica Neue", sans-serif;
           font-weight: 900;
           font-size: 40px;
           margin-bottom: 10px;
         }
         p {
-          color: #404F5E;
+          color: #ff0000;
           font-family: "Nunito Sans", "Helvetica Neue", sans-serif;
           font-size:20px;
           margin: 0;
         }
       i {
-        color: #9ABC66;
-        font-size: 100px;
-        line-height: 200px;
-        margin-left:-15px;
+        color: #ff0000;
+        font-size: 200px;
       }
       .card {
         background: white;
         padding: 60px;
-        border-radius: 4px;
+        border-radius: 10px;
         box-shadow: 0 2px 3px #C8D0D8;
         display: inline-block;
         margin: 0 auto;
@@ -39,10 +38,10 @@
     <body>
       <div class="card">
       <div style="border-radius:200px; height:200px; width:200px; background: #F8FAF5; margin:0 auto;">
-        <i class="checkmark">✓</i>
+        <i class="fa-solid fa-xmark"></i>
       </div>
-        <h1>Pemberitahuan</h1> 
-        <p>Segera lakukan pembayaran<br/> Halaman ini akan segera tertutup!</p>
+        <h1>Gagal</h1> 
+        <p>Maaf, pembayaran anda gagal!<br/> Silahkan kembali ke aplikasi untuk detail lebih lanjut!</p>
       </div>
     </body>
 </html>
