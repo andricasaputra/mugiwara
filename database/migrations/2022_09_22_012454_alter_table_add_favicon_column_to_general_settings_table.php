9ee2517831d01a4e12c1922848a0c7f552b37737
bayudava29 <bayudava16@gmail.com>
2022-09-22 12:05:58 +0700

    update

--- database/migrations/2022_09_22_012454_alter_table_add_favicon_column_to_general_settings_table.php
+++ database/migrations/2022_09_22_012454_alter_table_add_favicon_column_to_general_settings_table.php
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('general_settings', function (Blueprint $table) {
-            //
+            $table->dropColumn('favicon');
         });
     }
 };
