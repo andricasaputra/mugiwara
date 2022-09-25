088baebbedee661371c5fe10106c9ace801d3a0c
andricasaputra <dric.dev@gmail.com>
2022-09-25 15:06:42 +0800

    fixing bugs

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -5,7 +5,7 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Page Not Found</title>
+  <title>Internal Server Error</title>
   <!-- plugins:css -->
   <link rel="stylesheet" href="{{ ('assets/vendors/feather/feather.css') }}">
   <link rel="stylesheet" href="{{ ('assets/vendors/ti-icons/css/themify-icons.css') }}">
