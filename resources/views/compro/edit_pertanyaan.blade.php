b8345e6e3ae5641c7f5e002485aa84ed70e43aec
andricasaputra <dric.dev@gmail.com>
2022-09-27 00:11:04 +0800

    fixing bugs

--- resources/views/compro/edit_pertanyaan.blade.php
+++ resources/views/compro/edit_pertanyaan.blade.php
@@ -64,8 +64,7 @@
 </div>
 @endsection
 @push('scripts')
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
 <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.js"></script>
 <script>
     $(document).ready(function() {
@@ -86,6 +85,8 @@
               ['view', ['fullscreen', 'codeview', 'help']]
               ]
           });
+
+        $('.dropdown-toggle').dropdown()
     });
 </script>
 @endpush
