9706cdb643766d0f7e03da00f731c7ffd5c4a1ba
andricasaputra <dric.dev@gmail.com>
2022-10-15 02:13:44 +0800

    fix bugs

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -78,7 +78,7 @@ class PaymentController extends Controller
 
             $voucher = Voucher::find($request->voucher_id);
 
-            return $voucher->expires_at < now();
+            return $voucher;
 
             if($voucher->expires_at < now()){
                 return response()->json([
