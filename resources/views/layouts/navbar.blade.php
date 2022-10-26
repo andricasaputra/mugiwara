00b58306dff7c8131820c44ba73293a745d238b6
andricasaputra <dric.dev@gmail.com>
2022-10-26 23:25:10 +0800

    fixing bugs

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -2,7 +2,7 @@
       <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
         @role('admin')
           <a class="navbar-brand brand-logo mr-5" href="{{ route('admin.dashboard.index') }}"><img src="{{ asset('assets/images/capsuleinnlogo.png') }}" class="mr-2" alt="logo"/></a>
-          <a class="navbar-brand brand-logo-mini" href="{{ route('dashboard') }}"><img src="{{ asset('assets/images/capsuleinnlogo.png') }}" alt="logo"/></a>
+          <a class="navbar-brand brand-logo-mini" href="{{ route('admin.dashboard.index') }}"><img src="{{ asset('assets/images/capsuleinnlogo.png') }}" alt="logo"/></a>
         @else
           <a class="navbar-brand brand-logo mr-5" href="{{ route('dashboard') }}"><img src="{{ asset('assets/images/capsuleinnlogo.png') }}" class="mr-2" alt="logo"/></a>
           <a class="navbar-brand brand-logo-mini" href="{{ route('dashboard') }}"><img src="{{ asset('assets/images/capsuleinnlogo.png') }}" alt="logo"/></a>
